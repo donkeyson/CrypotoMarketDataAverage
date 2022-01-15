600dd1c2ef50ef9196a6efe3fbe9a7e2638572c6
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:39:20 +0900

    Update MVRVratio.py

--- conditions/MarketIndicator/MVRVratio.py
+++ conditions/MarketIndicator/MVRVratio.py
@@ -3,10 +3,10 @@
 con = int(input('input value of MVRV Ratio'))
 
 if (con > 3):
-  sum -= 2
+  sum.append(-2)
 elif (con > 2):
-  sum -= 1
+  sum.append(-1)
 elif (con > 1):
-  sum += 1
+  sum.append(1)
 elif (1 >= con):
-  sum += 2
+  sum.append(2)
