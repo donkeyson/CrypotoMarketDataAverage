ad1035e4ad8e6d06b477dba7535de855c29c492b
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:04:43 +0900

    Create exchangeStableRatio.py

---
+++ conditions/flow/exchangeStableRatio.py
@@ -0,0 +1,10 @@
+//크립토 퀀트
+
+con = input('Is Exchange Stablecoins Ratio growing? say it yes or no')
+
+if (con == 'yes'):
+  sum -= 1
+elif (con == 'no'):
+  sum += 1
+else:
+  print('say it yes or no')
