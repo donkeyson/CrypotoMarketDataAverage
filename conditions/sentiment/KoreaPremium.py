38795ee658ddd712dd580d3bdf9f2a677dcb0128
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:30:57 +0900

    Create KoreaPremium.py

---
+++ conditions/sentiment/KoreaPremium.py
@@ -0,0 +1,17 @@
+//크립토 퀀트
+
+con = int(input('input value of Korea Premium Index'))
+
+if (con >= 9):
+  sum -= 3
+elif (9 > con >= 6):
+  sum -= 2
+elif (6 > con >= 4):
+  sum -= 1
+elif (4 > con >= 2):
+  print('')
+elif (2 > con >= 0):
+  sum += 1
+elif (0 > con)
+  sum += 3
+  print('strong buy signal on Kimchi Premium')
