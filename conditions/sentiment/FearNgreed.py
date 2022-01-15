deecb88abbcdab45c84a69402d31c699d7a97c0a
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:41:40 +0900

    Update FearNgreed.py

--- conditions/sentiment/FearNgreed.py
+++ conditions/sentiment/FearNgreed.py
@@ -3,16 +3,16 @@
 con = int(input('input value of Fear & Greed Index'))
 
 if (con >= 80):
-  sum -= 3
+  sum.append(-3)
   print('strong sell signal in Fear and Greed Index')
 elif (80 > con >= 60):
-  sum -= 2
+  sum.append(-2)
 elif (60 > con >= 40):
-  sum -= 1
+  sum.append(-1)
 elif (40 > con >= 30):
   print('')
 elif (30 > con >= 20):
-  sum += 1
+  sum.append(1)
 elif (20 > con)
-  sum += 2
+  sum.append(2)
   print('strong buy signal on Fear and Greed Index')
