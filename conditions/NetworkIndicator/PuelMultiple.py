c2541d4e9609210364af152d7ae07d123f3da625
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:20:06 +0900

    Create PuelMultiple.py

---
+++ conditions/NetworkIndicator/PuelMultiple.py
@@ -0,0 +1,12 @@
+//크립토 퀀트
+
+con = int(input('input value of PuelMultiple'))
+
+if (con >= 2):
+  sum -= 2
+elif (2> con >= 1.5):
+  sum -= 1
+elif (1.5 > con >= 1):
+  print('')
+elif (con > 1):
+  sum += 2
