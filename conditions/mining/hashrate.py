92878127dbfde195f076c90782d4d334bfa2beaf
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:40:53 +0900

    Update hashrate.py

--- conditions/mining/hashrate.py
+++ conditions/mining/hashrate.py
@@ -1,8 +1,8 @@
 //해쉬레이트
 con = 'is hash rate under the price? say it yes or no'
 if (con == 'yes'):
-  sum -= 1
+  sum.append(-1)
 elif (con == 'no'):
-  sum += 1
+  sum.append(1)
 else:
   print('say it yes or no')
