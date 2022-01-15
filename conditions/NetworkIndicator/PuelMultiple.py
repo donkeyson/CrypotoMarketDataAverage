652c2cfbf15a3eb720777a81f41ecc16a1c5b209
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 15:39:58 +0900

    Update PuelMultiple.py

--- conditions/NetworkIndicator/PuelMultiple.py
+++ conditions/NetworkIndicator/PuelMultiple.py
@@ -3,10 +3,10 @@
 con = int(input('input value of PuelMultiple'))
 
 if (con >= 2):
-  sum -= 2
+  sum.append(-2)
 elif (2> con >= 1.5):
-  sum -= 1
+  sum.append(-1)
 elif (1.5 > con >= 1):
   print('')
 elif (con > 1):
-  sum += 2
+  sum.append(2)
