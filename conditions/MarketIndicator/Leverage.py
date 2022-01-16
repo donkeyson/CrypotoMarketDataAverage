5452a6270bc43d4a418e9385e673614329774786
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-16 15:45:52 +0900

    Update Leverage.py

--- conditions/MarketIndicator/Leverage.py
+++ conditions/MarketIndicator/Leverage.py
@@ -1,10 +1,13 @@
-//크립토 퀀트
-
 con = input('Is Leverage Ratio growing? say it yes or no')
+con2 = input('Is Price trend low? say it yes or no')
 
-if (con == 'yes'):
-  sum.append(-1)
-elif (con == 'no'):
+if ((con == 'yes') and (con2 == 'yes')):
   sum.append(1)
+elif ((con == 'yes') and (con2 == 'no')):
+  sum.append(-1)
+elif ((con == 'no') and (con2 == 'yes')):
+  print('')
+elif ((con == 'no') and (con2 == 'no')):
+  print('')
 else:
   print('say it yes or no')
