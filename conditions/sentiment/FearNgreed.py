9e9c87d73e0522ff08b7ee849d6a92ca3c914e2e
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-16 15:54:18 +0900

    Update FearNgreed.py

--- conditions/sentiment/FearNgreed.py
+++ conditions/sentiment/FearNgreed.py
@@ -13,6 +13,6 @@ elif (40 > con >= 30):
   print('')
 elif (30 > con >= 20):
   sum.append(1)
-elif (20 > con)
+elif (20 > con):
   sum.append(2)
   print('strong buy signal on Fear and Greed Index')
